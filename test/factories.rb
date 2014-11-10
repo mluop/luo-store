8ece23e5b02f9c2adbee724c69e06442c965b461
Michael Luo <mrl233@cornell.edu>
2014-11-10 01:45:14 -0800

    Started testing framework

--- test/factories.rb
+++ test/factories.rb
@@ -1,4 +1,15 @@
 FactoryGirl.define do 
   factory :user do
-    name
+    email {generate(:email)}
+    trait :admin do
+      is_admin true
+    end
+  end
+  sequence :email do |n|
+    "holyemail#{n}@example.com"
+  end
+  factory :product do
+    name "great"
+    price 0.23
+  end
 end
